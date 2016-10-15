0a7b1342e9131d71bfb1efe978eb6653706f327c
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-10-15 20:18:50 +0900

    watothonのリクエストサンプル

--- server/http_request.js
+++ server/http_request.js
@@ -3,19 +3,22 @@ var request = require('request');
 
 //ヘッダーを定義
 var headers = {
-  'Content-Type':'application/json;charset=UTF-8',
+  'Content-Type':'application/json;charset=UTF-8'
 }
+
+var username = '7743d7b8-b987-42a9-b2bc-3abd6aec7884';
+var password = 'OPCwL3dOfQoi';
 
 //オプションを定義
 var options = {
-  url: 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19&text=こんにちは',
+  url: 'https://' + username + ':' + password + '@gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19&text=こんにちは',
   method: 'GET',
   headers: headers,
-  json: true,
+  json: true
 }
 
 //リクエスト送信
 request(options, function (error, response, body) {
-  console.log(body);
+  console.log(JSON.stringify(body));
   //コールバックで色々な処理
 })
