ab3eaacb8a82e5dd5fbd84f77675253b7e5badfd
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-10-16 10:09:27 +0900

    とりあえず、tweetが次々取れるMのは作った

--- server/crawler.js
+++ server/crawler.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var Sequelize = require ('sequelize');
 var dbInfo = JSON.parse(fs.readFileSync('./config/database.json', 'utf8'));
-var sequelize = new Sequelize(dbInfo.prod.database, dbInfo.prod.user, dbInfo.prod.password, { host: dbInfo.prod.host, port: 3306 })
+var sequelize = new Sequelize(dbInfo.prod.database, dbInfo.prod.user, dbInfo.prod.password, { host: dbInfo.prod.host, port: 3306, benchmark: true})
 
 var emojiStrip = require('emoji-strip');
 
@@ -13,6 +13,7 @@ var output_sentences = sequelize.define('output_sentences', {
       scoreKind: Sequelize.STRING,
       score: Sequelize.FLOAT,
 });
+output_sentences.sync();
 
 var output_sentence_meta = sequelize.define('output_sentence_meta', {
       sentenceId: Sequelize.INTEGER,
@@ -22,45 +23,74 @@ var output_sentence_meta = sequelize.define('output_sentence_meta', {
       sourceUserId: Sequelize.STRING,
       sourceUserName: Sequelize.STRING,
 });
+output_sentence_meta.sync();
 
 var twitterClient = new twitter(apiconfigInfo.twitter);
-var options = {q: "オブラート", count: 100};
 
-var ranges = [
-        '\ud83c[\udf00-\udfff]',
-        '\ud83d[\udc00-\ude4f]',
-        '\ud83d[\ude80-\udeff]',
-        '\ud7c9[\ude00-\udeff]',
-        '[\u2600-\u27BF]'
-];
-var ex = new RegExp(ranges.join('|'), 'g');
+//3秒間スリープ
+//sleep.sleep(n);
+var roopCount = 0;
 
-twitterClient.get('search/tweets', options, function(error, tweets, response){
-  console.log(JSON.stringify(tweets));
-  var tweetCount = tweets.statuses.length || 0;
-  var bulkSenteces = [];
-  var bulkSenteceMetas = [];
-  for(var i = 0;i < tweetCount;++i){
-    var sen = {};
-    var meta = {};
-    sen.scoreKind = "";
-    sen.score = 0;
-    // 絵文字を除去
-    sen.sentence = emojiStrip(tweets.statuses[i].text);
-    bulkSenteces.push(sen);
+var isExecute = false;
+var isFinish = false;
 
-    meta.sentenceId = i;
-    var texts = [];
-    tweets.statuses[i].entities.hashtags.forEach(function(h){
-    	texts.push(h.text);
-    })
-    meta.tag = texts.join(",");
-    meta.sourceId = tweets.statuses[i].id + '';
-    meta.sourceRootUrl = "https://twitter.com/";
-    meta.sourceUserId = tweets.statuses[i].user.id + '';
-    meta.sourceUserName = tweets.statuses[i].user.screen_name;
-    bulkSenteceMetas.push(meta);
+var async = require('async');
+
+setInterval(function() {
+  if(isExecute) return;
+  isExecute = true;
+  console.log(roopCount);
+  var lastId = -1;
+  var options = {q: "オブラート", count: 100};
+  if(lastId > 0){
+  	options.max_id = lastId;
   }
-  output_sentences.bulkCreate(bulkSenteces);
-  output_sentence_meta.bulkCreate(bulkSenteceMetas);
-});
+
+
+    	console.log("start");
+    	twitterClient.get('search/tweets', options, function(error, tweets, response){
+  			console.log(roopCount);
+    		var tweetCount = tweets.statuses.length || 0;
+    		var bulkSenteces = [];
+    		var bulkSenteceMetas = [];
+    	for(var i = roopCount;i < tweetCount;++i){
+    	  var sen = {};
+      	  var meta = {};
+      	  sen.scoreKind = "";
+	      sen.score = 0;
+	      // 絵文字を除去
+	      sen.sentence = emojiStrip(tweets.statuses[i].text);
+	      bulkSenteces.push(sen);
+
+	      meta.sentenceId = i;
+	      var texts = [];
+	      tweets.statuses[i].entities.hashtags.forEach(function(h){
+	    	  texts.push(h.text);
+	      })
+    	  meta.tag = texts.join(",");
+    	  meta.sourceId = tweets.statuses[i].id + '';
+    	  meta.sourceRootUrl = "https://twitter.com/";
+    	  meta.sourceUserId = tweets.statuses[i].user.id + '';
+    	  meta.sourceUserName = tweets.statuses[i].user.screen_name;
+    	  bulkSenteceMetas.push(meta);
+    	  if(tweetCount - 1 == i){
+    	  	lastId = tweets.statuses[i].id;
+    	  }
+    	}
+    	output_sentences.bulkCreate(bulkSenteces).error(function(err) {
+          // この例だとa@c4とabc5がエラーになる
+          console.log(JSON.stringify(err));
+        });
+    	output_sentence_meta.bulkCreate(bulkSenteceMetas).error(function(err) {
+          // この例だとa@c4とabc5がエラーになる
+          console.log(JSON.stringify(err));
+        });
+    	roopCount = roopCount + tweetCount;
+    	isExecute = false;
+    	if(tweetCount < 100){
+    	  isFinish = true;
+    	}
+    	var query = tweets.search_metadata.next_results.substring(1);
+    	options = JSON.parse('{"' + decodeURI(query).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
+      });
+}, 1000);
