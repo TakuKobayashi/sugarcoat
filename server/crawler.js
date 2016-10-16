262c6b518aa3667f12d11b80c287c22d1c993153
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-10-16 10:31:02 +0900

    intent詰めました

--- server/crawler.js
+++ server/crawler.js
@@ -46,51 +46,50 @@ setInterval(function() {
   	options.max_id = lastId;
   }
 
+  console.log("start");
+  twitterClient.get('search/tweets', options, function(error, tweets, response){
+  	console.log(roopCount);
+    var tweetCount = tweets.statuses.length || 0;
+    var bulkSenteces = [];
+    var bulkSenteceMetas = [];
+    for(var i = roopCount;i < tweetCount;++i){
+      var sen = {};
+      var meta = {};
+      sen.scoreKind = "";
+	  sen.score = 0;
+	  // 絵文字を除去
+	  sen.sentence = emojiStrip(tweets.statuses[i].text);
+	  bulkSenteces.push(sen);
 
-    	console.log("start");
-    	twitterClient.get('search/tweets', options, function(error, tweets, response){
-  			console.log(roopCount);
-    		var tweetCount = tweets.statuses.length || 0;
-    		var bulkSenteces = [];
-    		var bulkSenteceMetas = [];
-    	for(var i = roopCount;i < tweetCount;++i){
-    	  var sen = {};
-      	  var meta = {};
-      	  sen.scoreKind = "";
-	      sen.score = 0;
-	      // 絵文字を除去
-	      sen.sentence = emojiStrip(tweets.statuses[i].text);
-	      bulkSenteces.push(sen);
-
-	      meta.sentenceId = i;
-	      var texts = [];
-	      tweets.statuses[i].entities.hashtags.forEach(function(h){
-	    	  texts.push(h.text);
-	      })
-    	  meta.tag = texts.join(",");
-    	  meta.sourceId = tweets.statuses[i].id + '';
-    	  meta.sourceRootUrl = "https://twitter.com/";
-    	  meta.sourceUserId = tweets.statuses[i].user.id + '';
-    	  meta.sourceUserName = tweets.statuses[i].user.screen_name;
-    	  bulkSenteceMetas.push(meta);
-    	  if(tweetCount - 1 == i){
-    	  	lastId = tweets.statuses[i].id;
-    	  }
-    	}
-    	output_sentences.bulkCreate(bulkSenteces).error(function(err) {
-          // この例だとa@c4とabc5がエラーになる
-          console.log(JSON.stringify(err));
-        });
-    	output_sentence_meta.bulkCreate(bulkSenteceMetas).error(function(err) {
-          // この例だとa@c4とabc5がエラーになる
-          console.log(JSON.stringify(err));
-        });
-    	roopCount = roopCount + tweetCount;
-    	isExecute = false;
-    	if(tweetCount < 100){
-    	  isFinish = true;
-    	}
-    	var query = tweets.search_metadata.next_results.substring(1);
-    	options = JSON.parse('{"' + decodeURI(query).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
-      });
+	  meta.sentenceId = i;
+	  var texts = [];
+	  tweets.statuses[i].entities.hashtags.forEach(function(h){
+	    texts.push(h.text);
+	  })
+      meta.tag = texts.join(",");
+      meta.sourceId = tweets.statuses[i].id + '';
+      meta.sourceRootUrl = "https://twitter.com/";
+      meta.sourceUserId = tweets.statuses[i].user.id + '';
+      meta.sourceUserName = tweets.statuses[i].user.screen_name;
+      bulkSenteceMetas.push(meta);
+      if(tweetCount - 1 == i){
+      	lastId = tweets.statuses[i].id;
+      }
+    }
+    output_sentences.bulkCreate(bulkSenteces).error(function(err) {
+      // この例だとa@c4とabc5がエラーになる
+      console.log(JSON.stringify(err));
+    });
+    output_sentence_meta.bulkCreate(bulkSenteceMetas).error(function(err) {
+      // この例だとa@c4とabc5がエラーになる
+      console.log(JSON.stringify(err));
+    });
+    roopCount = roopCount + tweetCount;
+    isExecute = false;
+    if(tweetCount < 100){
+      isFinish = true;
+    }
+    var query = tweets.search_metadata.next_results.substring(1);
+    options = JSON.parse('{"' + decodeURI(query).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
+  });
 }, 1000);
